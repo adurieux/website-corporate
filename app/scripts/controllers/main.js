697d8d8f30141779684c630735cb86787fa53ac2
Antoine Durieux <adurieux@stanford.edu>
2014-04-21 13:51:45 +0200

    refactor(app) : formatting.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-angular.module('websiteCorporateApp')
-    .controller('MainCtrl', function ($scope) {
-        
-    });
+angular.module('websiteCorporateApp').controller('MainCtrl', function ($scope) {
+
+});
