16e7ceb2b287f87075c8d1c81c63541d3be040bf
Antoine Durieux <adurieux@stanford.edu>
2014-07-13 18:02:46 +0200

    feature(service) : added pages.

---
+++ app/src/mailinglist/mailinglist.controller.js
@@ -0,0 +1,35 @@
+/**
+ * Modal that allows the user to register on the mailing list
+ */
+var MailingListModalController = function ($scope, $modalInstance, $http) {
+
+    $scope.mailingListRecord = {
+        origin: 0,   // Website Corporate Alkemics
+        email: null,
+        message: "Merci de bien vouloir m'inscrire à votre mailing list.",
+    };
+
+    /*
+     * Register a user to the mailing list
+     * Default is a POST to the endpoint
+     * Fallback with a GET to have the email in the logs
+     */
+    $scope.ok = function () {
+        var record = $scope.mailingListRecord;
+        $http.post(
+            'https://auth.alkemics.com/auth/v1/mailinglist/register',
+            record
+        ).success(function (response) {
+            $modalInstance.close($scope.product);
+        }).error(function (response) {
+            $http.get(
+                'https://auth.alkemics.com/auth/v1/mailinglist/register',
+                record
+            );
+            $modalInstance.close($scope.product);
+        });
+    };
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
---
+++ app/src/technology/technology.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('websiteCorporateApp').controller('TechnologyCtrl', [
+    '$scope',
+    function ($scope) {
+
+}]);
