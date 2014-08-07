8105f7195371148e97a127da29ec6e27811b3a5f
Antoine Durieux <adurieux@stanford.edu>
2014-08-07 17:13:35 +0200

    fix(grunt) : changed port to avoid port conflict.

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,10 +61,10 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9005,
+        port: 9006,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost.alkemics.com',
-        livereload: 357005
+        livereload: 357006
       },
       livereload: {
         options: {
